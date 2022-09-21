491a79ed02075d63d4e976762b49102f4a8f060b
KasiViswanadh119 <113752734+KasiViswanadh119@users.noreply.github.com>
2022-09-21 11:50:38 +0530

    a=int(input()) sum=0 for i in range(1,a+1):     if a%i==0:         if i==a:             break         sum=sum+i if a==sum:     print(True) else:     print(False)

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -2,10 +2,10 @@ a=int(input())
 sum=0
 for i in range(1,a+1):
     if a%i==0:
-        if a==i:
+        if i==a:
             break
         sum=sum+i
-if sum==a:
-    print('True')
+if a==sum:
+    print(True)
 else:
-    print('False')
+    print(False)
